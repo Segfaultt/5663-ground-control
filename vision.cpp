5a7b2921aef694ccdb3ffc7a71e9042e5a5aaa26
ShrimpKeeper <xophnex@gmail.com>
2017-01-12 15:47:09 +0000

    added white midpoint finding

--- vision.cpp
+++ vision.cpp
@@ -1,6 +1,24 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 
+//find the centreline where the pin is
+unsigned int Robot::average_y_coordinate(Mat& matrix)
+{
+	unsigned long long cummulative_value = 0, n_of_values = 0;
+
+        for(int i = 0; i < matrix.rows; ++i) {
+                const unsigned char* row = matrix.ptr<unsigned char>(i);
+                for(int j = 0; j < matrix.cols; j++) {
+                        if (row[j] == 255) {
+                                cummulative_value += j;
+                                n_of_values++;
+                        }
+                }
+        }
+
+        return cummulative_value/n_of_values;
+}
+
 void Robot::vision(const double thresh, const double max_val)
 {
 	//set up camera input (may need to be changed)
