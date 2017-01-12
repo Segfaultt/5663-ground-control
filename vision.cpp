c22c1abde832774db5c6c4d0751a1b6ff391f887
ShrimpKeeper <xophnex@gmail.com>
2017-01-12 12:47:49 +0000

    updated vision.cpp

--- vision.cpp
+++ vision.cpp
@@ -1,9 +1,13 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 
+int average_y_coord(Mat matrix) {
+	int cummulative_value;
+}
+
 void Robot::vision(const double thresh, const double max_val)
 {
-	//set up camera input
+	//set up camera input (may need to be changed)
 	cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture();
         camera.SetResolution(640, 480);
         cs::CvSink cvSink = CameraServer::GetInstance()->GetVideo();
@@ -14,6 +18,7 @@ void Robot::vision(const double thresh, const double max_val)
         	cvSink.GrabFrame(image);
 
 		//apply threshold to get with_thresh
-		cv::threshhold(image, with_thresh, thresh, max_val, THRESH_BINARY);
+		cv::threshold(image, with_thresh, thresh, max_val, THRESH_BINARY);//this should work in theory
         }
 }
+
